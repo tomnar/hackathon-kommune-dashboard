dc714b8fe34b66ee28624d784800062fa6100595
tomnar <tomquast89@gmail.com>
2014-02-11 22:33:37 +0100

    added behandlingsinforamation

--- js/main.js
+++ js/main.js
@@ -16,6 +16,11 @@ $(document).ready(function(){
 		$(oldBig.find('.small')).show();
 		$(oldBig.find('.big')).hide();
 		
+		//reset animations in other boxes
+		$('.box5 .imageLineup .el').hide();
+		$('.box5 .line').css('height', 0);
+		$('.box5 .imageLineup .el div').hide();
+		
 		$(this).addClass('is-expanded');
 		$($(this).find('.small')).hide();
 		$($(this).find('.big')).show();
@@ -25,5 +30,19 @@ $(document).ready(function(){
 	$('header li').click(function(){
 		var filter = $(this).attr("class");
 		$container.isotope({ filter: '.' + filter });
+	});
+	
+	$('.box5').click(function(){
+		$('.box5 .line').animate({
+			height: 396
+		},2000);
+		$('.box5 .imageLineup .el').each( function(i){
+			$(this).delay(300*i).fadeIn(200);
+		});
+	});
+	$('.box5 .imageLineup .el').click(function(e){
+		e.stopPropagation();
+		$('.box5 .imageLineup .el div').hide();
+		$(this).find('div').fadeIn(300);
 	});
 });
