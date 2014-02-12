b0f2714be3c68d2c1c66753aba71ae15cf4e8986
tomnar <tomquast89@gmail.com>
2014-02-12 11:28:31 +0100

    Added interaction to the smerte view

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 		$('.box5 .imageLineup .el').hide();
 		$('.box5 .imageLineup .el div').hide();
 		$('.box2 .big img').attr('src', 'img/Personclean.png');
+		clearTexts();
 		
 		$(this).addClass('is-expanded');
 		$($(this).find('.small')).hide();
@@ -35,15 +36,23 @@ $(document).ready(function(){
 	
 	$('.box2 .c1').click(function(){
 		$('.box2 .big img').attr('src', 'img/Person1.png');
+		clearTexts();
+		$('.box2 .big .text.a').show();
 	});
 	$('.box2 .c2').click(function(){
 		$('.box2 .big img').attr('src', 'img/Person2.png');
+		clearTexts();
+		$('.box2 .big .text.b').show();
 	});
 	$('.box2 .c3').click(function(){
 		$('.box2 .big img').attr('src', 'img/Person3.png');
+		clearTexts();
+		$('.box2 .big .text.c').show();
 	});
 	$('.box2 .c4').click(function(){
 		$('.box2 .big img').attr('src', 'img/Person4.png');
+		clearTexts();
+		$('.box2 .big .text.a').show();
 	});
 	
 	$('.box5').click(function(){
@@ -56,4 +65,10 @@ $(document).ready(function(){
 		$('.box5 .imageLineup .el div').hide();
 		$(this).find('div').fadeIn(300);
 	});
+	
+	function clearTexts(){
+		$('.box2 .big .text.a').hide();
+		$('.box2 .big .text.b').hide();
+		$('.box2 .big .text.c').hide();
+	}
 });
